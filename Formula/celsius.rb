82ccd2aac433ec1137baf42ba27f14dcc5e74ccf
Luis Markmann <110569833+lmarkmann@users.noreply.github.com>
2026-06-10 14:58:09 +0100

    celsius v0.3.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/celsius.rb
+++ Formula/celsius.rb
@@ -1,8 +1,8 @@
 class Celsius < Formula
   desc "Terminal weather as a first-person sky view"
   homepage "https://github.com/lmarkmann/celsius"
-  url "https://codeload.github.com/lmarkmann/celsius/tar.gz/refs/tags/v0.3.1"
-  sha256 "ccd3685d807a5019d70f6f5ead411b4abdecdd2ec1e0cf497c4beb944aa20247"
+  url "https://codeload.github.com/lmarkmann/celsius/tar.gz/refs/tags/v0.3.2"
+  sha256 "bdbe31074857dd12ab662d2da29023c82f84ac7f9a4935d7f8e17df06ad0e472"
   license "MIT"
   head "https://github.com/lmarkmann/celsius.git", branch: "main"
 
